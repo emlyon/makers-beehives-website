396f4b21610a620669623a5eab0213be247c9c59
RADISSON lionel <lionel.radisson@gmail.com>
2017-05-31 14:37:20 +0200

    adding lazy load + styling

--- server/static/assets/js/main.js
+++ server/static/assets/js/main.js
@@ -1,1 +1,15 @@
+const resizeCards = e => {
+    const cards = document.querySelectorAll( '#home .card' );
+    cards.forEach( d => $( d ).height( 'auto' ) );
+    
+    let h = 0;
+    cards.forEach( d => {
+        let tmp = parseInt( $( d ).height() );
+        h = tmp > h ? tmp : h;
+    } );
 
+    cards.forEach( d => $( d ).height( h + 'px' ) );
+};
+
+window.addEventListener( 'load', resizeCards );
+window.addEventListener( 'resize', resizeCards );
