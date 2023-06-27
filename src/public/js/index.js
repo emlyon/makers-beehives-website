b25bc9cb6d2641f2e152988d429b88fed61ac111
Gaël <gbarnabe@em-lyon.com>
2023-06-27 12:48:42 +0200

    Design Map area

--- src/public/js/index.js
+++ src/public/js/index.js
@@ -1,5 +1,5 @@
 $(function () {
   // beehivesData is a global variable defined in the ejs template
-  createChart(beehivesData['bee2'].data, '#bee2Chart');
-  createChart(beehivesData['bee1'].data, '#bee1Chart');
+  createChart(beehivesData['bee2'].data, '.bee2Chart');
+  createChart(beehivesData['bee1'].data, '.bee1Chart');
 });
