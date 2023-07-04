492fc6c3efccc38a3172477faf7aecb66c6cbe48
Gaël <gbarnabe@em-lyon.com>
2023-07-04 17:18:20 +0200

    Test

--- src/public/js/index.js
+++ src/public/js/index.js
@@ -1,5 +1,6 @@
 $(function () {
   // beehivesData is a global variable defined in the ejs template
+  
   createChart(beehivesData['bee3'].data, '.bee4Chart');
   createChart(beehivesData['bee3'].data, '.bee3Chart');
   createChart(beehivesData['bee2'].data, '.bee2Chart');
