1aa31b89689f2de7d15506994d0acc1ec4ce2d39
Gaël <gbarnabe@em-lyon.com>
2023-07-04 16:58:26 +0200

    fix #12

--- src/public/js/index.js
+++ src/public/js/index.js
@@ -1,5 +1,7 @@
 $(function () {
   // beehivesData is a global variable defined in the ejs template
+  createChart(beehivesData['bee3'].data, '.bee4Chart');
+  createChart(beehivesData['bee3'].data, '.bee3Chart');
   createChart(beehivesData['bee2'].data, '.bee2Chart');
   createChart(beehivesData['bee1'].data, '.bee1Chart');
 });
